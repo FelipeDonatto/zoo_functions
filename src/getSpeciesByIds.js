a44bfe2d7f33dceadfa68f03025b046c17d3c868
Felipe <fe.donatto@outlook.com>
2022-05-20 16:30:17 -0300

    Implemente a função getSpeciesByIds concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+// const zoo_data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...str) {
+  return str.map((element) => species.find(({ id }) => id === element));
 }
+// nota mental - javascript é estranho
 
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
 module.exports = getSpeciesByIds;

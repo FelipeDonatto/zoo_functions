0dad8fc1872c50257a9d12b1575927747e7ed2b1
Felipe <fe.donatto@outlook.com>
2022-05-20 18:34:25 -0300

    Implemente a função getRelatedEmployees concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+
+function isManager(personId) {
+  const managerCheck = managers.some((element) => element === personId);
+  return managerCheck;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const related = data.employees.filter((element) => element.managers.includes(managerId))
+      .map((element) => `${element.firstName} ${element.lastName}`);
+    return related;
+  }
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
+console.log(getRelatedEmployees(stephanieId));
 module.exports = { isManager, getRelatedEmployees };

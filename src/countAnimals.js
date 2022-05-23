d43cf80c9d744311a8c084869d9a6c99b950b9bb
Felipe <fe.donatto@outlook.com>
2022-05-23 17:27:37 -0300

    count animals concluido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui.
+function countAnimals(obj) {
+  if (typeof obj !== 'object') {
+    return species.reduce((accumulator, index) => {
+      accumulator[index.name] = index.residents.length;
+      return accumulator;
+    }, {});
+  }
+  const objSize = Object.keys(obj).length;
+  const animal = species.find((element) => element.name === obj.specie);
+  if (objSize === 1) {
+    return animal.residents.length;
+  }
+  const sex = animal.residents.filter((element) => element.sex === obj.sex);
+  return sex.length;
 }
 
+// ficou meio confuso mas deu certo
+// console.log(countAnimals({ specie: 'bears', sex: 'male' }));
 module.exports = countAnimals;
